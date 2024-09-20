22816077f5b3ad90ad19b58a43a84717b77a99c0
haduigon <haduigon@gmail.com>
2024-09-21 00:43:08 +0300

    more styles were added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -37,16 +37,16 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center mx-auto flex flex-col md:flex-row ">
         <div className="text-white text-lg font-bold">Kochanet Blog</div>
         <button
-          onClick={handleAdd}
-          className="w-full md:w-auto mb-2 md:mb-0 md:ml-2 ml-2 bg-blue-500 text-white text-sm font-semibold py-1 px-3 rounded hover:bg-blue-600"
-        >
-          Add post
-        </button>
-        <button
           onClick={logout}
           className="w-full md:w-auto mb-2 md:mb-0 md:ml-2 ml-2 bg-blue-500 text-white text-sm font-semibold py-1 px-3 rounded hover:bg-blue-600"
         >
           Logout
+        </button>
+        <button
+          onClick={handleAdd}
+          className="w-full md:w-auto mb-2 md:mb-0 md:ml-2 ml-2 bg-blue-500 text-white text-sm font-semibold py-1 px-3 rounded hover:bg-blue-600"
+        >
+          Add post
         </button>
         <div>
           <select
